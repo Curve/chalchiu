77aa9271f940e8af2b3b3f02c65fe1b729bde87b
Curve <fynnbwdt@gmail.com>
2023-04-24 13:27:31 +0200

    chore: reformat examples

--- examples/coromon/money.mod.lua
+++ examples/coromon/money.mod.lua
@@ -1,13 +1,12 @@
 return {
-	name   		= 'Infinite Money',
+	name        = 'Infinite Money',
 	description = 'Gives you unlimited money',
-	
-	target 		= 'classes.modules.playerCurrency',
-	hook   = function (table)
+	target      = 'classes.modules.playerCurrency',
+	hook        = function(table)
 		local original = table.onLoadSaveslotData
 
 		table.onLoadSaveslotData = function(self, data)
-			data.gold = 2^52
+			data.gold = 2 ^ 52
 			return original(self, data)
 		end
 	end
--- examples/coromon/pimp_my_coromon.mod.lua
+++ examples/coromon/pimp_my_coromon.mod.lua
@@ -1,21 +1,20 @@
 return {
-	name   		= 'Pimp My Coromon!',
+	name        = 'Pimp My Coromon!',
 	description = 'Buffs all your coromon to max level!',
-	
-	target 		= 'classes.modules.playerMonsters',
-	hook   		= function (table)
+	target      = 'classes.modules.playerMonsters',
+	hook        = function(table)
 		local original = table.onLoadSaveslotData
 
 		table.onLoadSaveslotData = function(self, data)
 			local rtn = original(self, data)
-			
+
 			local squad = self:getSquad()
-			
+
 			for i = 1, #squad do
 				squad[i]:tryIncreaseLevel(100)
 				print("Pimped", squad[i]:getDisplayName())
 			end
-			
+
 			return rtn
 		end
 	end
