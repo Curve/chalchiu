7625652caf027921fb1ec0a8500148a8ca9b1513
Curve <fynnbwdt@gmail.com>
2023-04-27 18:35:38 +0200

    refactor(logger): `chalchiu.txt` -> `chalchiu.log`

--- src/logger.cpp
+++ src/logger.cpp
@@ -9,13 +9,14 @@ namespace chalchiu
 
     logger::logger()
     {
-        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("chalchiu.txt");
         auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("chalchiu.log");
 
-        m_logger = std::make_unique<spdlog::logger>("chalchiu", spdlog::sinks_init_list{console_sink, file_sink});
+        spdlog::sinks_init_list sink_list{console_sink, file_sink};
+        m_logger = std::make_unique<spdlog::logger>("chalchiu", sink_list);
 
-        m_logger->set_level(spdlog::level::trace);
         m_logger->flush_on(spdlog::level::trace);
+        m_logger->set_level(spdlog::level::trace);
     }
 
     spdlog::logger *logger::operator->()
